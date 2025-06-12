d5f2ca7784fe88b960fc7ff704f7612eb7172cb7
Shayan Rais <shayan.rais@disrupt.com>
2025-06-12 13:46:18 +0500

    [deployment] [augment] change port

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,14 +3,14 @@ const nextConfig = {
   output: "standalone",
   env: {
     NEXT_PUBLIC_API_URL:
-      process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+      process.env.NEXT_PUBLIC_API_URL || "http://localhost:8570", /** 8000 to 8570 */
   },
   async rewrites() {
     return [
       {
         source: "/api/:path*",
         destination: `${
-          process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"
+          process.env.NEXT_PUBLIC_API_URL || "http://localhost:8570" /** 8000 to 8570 */
         }/api/:path*`,
       },
     ];
